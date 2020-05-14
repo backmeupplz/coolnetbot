bb01764b4bc3ef67778c5dbf0c5c368f1c9b4113
backmeupplz <backmeupplz@gmail.com>
2020-05-14 07:59:24 -0700

    add disable preview

--- src/commands/network.ts
+++ src/commands/network.ts
@@ -12,18 +12,27 @@ export async function handleNetwork(ctx: TelegrafContext & UserProp) {
         password: ctx.dbuser.password,
       })
       return ctx.replyWithHTML(
-        `Вы подписаны на сообщество с паролем <code>${ctx.dbuser.password}</code>. На данный момент, количество людей в сообществе: ${number}. Ссылка для приглашения: ${networkUrl}. Каждые 2 дня бот будет присылать вам новый контакт. Спасибо!`
+        `Вы подписаны на сообщество с паролем <code>${ctx.dbuser.password}</code>. На данный момент, количество людей в сообществе: ${number}. Ссылка для приглашения: ${networkUrl}. Каждые 2 дня бот будет присылать вам новый контакт. Спасибо!`,
+        {
+          disable_web_page_preview: true,
+        }
       )
     }
     return ctx.replyWithHTML(
-      'Пожалуйста, пришлите сообщение в формате <code>/network пароль_коммьюнити</code>.'
+      'Пожалуйста, пришлите сообщение в формате <code>/network пароль_коммьюнити</code>.',
+      {
+        disable_web_page_preview: true,
+      }
     )
   }
   const networkUrl = `t.me/${ctx.botInfo.username}?start=${password}`
   const number = await UserModel.countDocuments({ password })
   if (ctx.dbuser.password === password) {
     return ctx.replyWithHTML(
-      `Вы уже подписаны на сообщество с паролем <code>${password}</code>. На данный момент, количество людей в сообществе: ${number}. Ссылка для приглашения: ${networkUrl}. Каждые 2 дня бот будет присылать вам новый контакт. Спасибо!`
+      `Вы уже подписаны на сообщество с паролем <code>${password}</code>. На данный момент, количество людей в сообществе: ${number}. Ссылка для приглашения: ${networkUrl}. Каждые 2 дня бот будет присылать вам новый контакт. Спасибо!`,
+      {
+        disable_web_page_preview: true,
+      }
     )
   }
   ctx.dbuser.password = password
@@ -33,6 +42,9 @@ export async function handleNetwork(ctx: TelegrafContext & UserProp) {
   return ctx.replyWithHTML(
     `Успех! Вы успешно подписались на сообщество с паролем <code>${password}</code>. На данный момент, количество людей в сообществе: ${
       number + 1
-    }. Ссылка для приглашения: ${networkUrl}. Каждые 2 дня бот будет присылать вам новый контакт. Спасибо!`
+    }. Ссылка для приглашения: ${networkUrl}. Каждые 2 дня бот будет присылать вам новый контакт. Спасибо!`,
+    {
+      disable_web_page_preview: true,
+    }
   )
 }
